079da45a63da74f83c00cf421bfd70b4d00034b3
chaobk <1004945427@qq.com>
2023-04-02 22:14:35 +0800

    服务统一网关，增加网关的拦截配置

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,13 @@
 package com.atguigu.gulimall.product.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Comparator;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +22,9 @@ import com.atguigu.gulimall.product.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+    @Autowired
+    CategoryDao categoryDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
@@ -24,6 +33,42 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // 1.查出所有分类
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        // 2.组装成父子的树形结构
+        List<CategoryEntity> level1Menu = entities.stream().filter(c -> c.getParentCid() == 0)
+                .map(m -> {
+                    m.setChildren(getChildren(m, entities));
+                    return m;
+                })
+                .sorted(Comparator.comparingInt(m -> (m.getSort() == null ? 0 : m.getSort())))
+                .collect(Collectors.toList());
+
+
+        return level1Menu;
+    }
+
+    /**
+     * 递归查找所有菜单的子菜单
+     *
+     * @param root
+     * @param all
+     * @return
+     */
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> categoryEntity.getParentCid() == root.getCatId())
+                .map(categoryEntity -> {
+                    categoryEntity.setChildren(getChildren(categoryEntity, all));
+                    return categoryEntity;
+                })
+                .sorted(Comparator.comparingInt(m -> (m.getSort() == null ? 0 : m.getSort())))
+                .collect(Collectors.toList());
+        return children;
     }
 
 }
