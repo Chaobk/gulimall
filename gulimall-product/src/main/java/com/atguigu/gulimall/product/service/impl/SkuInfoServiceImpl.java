45aab728f41590ea95947e18076da7fa1f19f857
chaobk <1004945427@qq.com>
2023-05-23 21:00:09 +0800

    sku查询

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,6 +1,8 @@
 package com.atguigu.gulimall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -11,6 +13,7 @@ import com.atguigu.common.utils.Query;
 import com.atguigu.gulimall.product.dao.SkuInfoDao;
 import com.atguigu.gulimall.product.entity.SkuInfoEntity;
 import com.atguigu.gulimall.product.service.SkuInfoService;
+import org.springframework.util.StringUtils;
 
 
 @Service("skuInfoService")
@@ -29,6 +32,59 @@ public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> i
     @Override
     public void saveSkuInfo(SkuInfoEntity skuInfoEntity) {
         this.baseMapper.insert(skuInfoEntity);
+    }
+
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> wrapper = new QueryWrapper<>();
+        //{
+        //page: 1,//当前页码
+        //limit: 10,//每页记录数
+        //sidx: 'id',//排序字段
+        //order: 'asc/desc',//排序方式
+        //key: '华为',//检索关键字
+        //catalogId: 0,
+        //brandId: 0,
+        //min: 0,
+        //max: 0
+        //}
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.and(w -> {
+               w.eq("sku_id", key).or().like("sku_name", key);
+            });
+        }
+
+        String catalogId = (String) params.get("catalogId");
+        if (!StringUtils.isEmpty(catalogId) && !"0".equals(catalogId)) {
+            wrapper.and(w -> w.eq("catalog_id", catalogId));
+        }
+
+        String brandId = (String) params.get("brandId");
+        if (!StringUtils.isEmpty(brandId) && !"0".equals(brandId)) {
+            wrapper.and(w -> w.eq("brand_id", brandId));
+        }
+        String min = (String) params.get("min");
+        String max = (String) params.get("max");
+        if (!StringUtils.isEmpty(min)) {
+            wrapper.and(w -> w.ge("price", min));
+        }
+        if (!StringUtils.isEmpty(max)) {
+            try {
+                BigDecimal bigDecimal = new BigDecimal(max);
+                if (bigDecimal.compareTo(new BigDecimal("0")) == 1) {
+                    wrapper.and(w -> w.le("price", max));
+                }
+            } catch (Exception e) {
+
+            }
+        }
+
+        IPage<SkuInfoEntity> page = this.page(
+                new Query<SkuInfoEntity>().getPage(params),
+                wrapper
+        );
+        return new PageUtils(page);
     }
 
 }
